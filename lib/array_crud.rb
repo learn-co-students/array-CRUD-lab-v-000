cd96319914b68fcc4322c8064c5d4debb0921177
Elizabeth Kuok <elizabethkuok@gmail.com>
2017-07-07 15:31:46 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,13 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
-  famous_cats << element
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
-  famous_cats.unshift(element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
