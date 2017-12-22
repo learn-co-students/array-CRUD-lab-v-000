b3ec4b505b05bb3446b51a556c6f8e96e2054eb3
Cam Nguyen <cnguyen1187@gmail.com>
2017-12-22 22:08:32 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["wow", "I", "am", "really"]
+  array = ["1", "2", "3", "4"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "arrays!"
+  array << "5"
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("wow")
+  array.unshift("0")
 end
 
 def remove_element_from_end_of_array(array)
