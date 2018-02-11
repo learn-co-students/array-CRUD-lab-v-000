26306d1190f3f3e989ffe0d117ce6c5e48db1a2e
Valencia Caldwell <vkcaldwell@gmail.com>
2018-02-11 18:35:00 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,41 @@
+'require pry'
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array = ["I", "am", "really", "learning", "arrays!"]
+  create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array=["wow", "I", "am", "really", "learning", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[-1]
 end
