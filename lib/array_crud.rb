5c81fe732d90a33276557cc87d326e5dc26a10f1
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-07 22:14:10 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array = ["red", "yellow", "purple", "blue"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
