0924589a2c249a4204d1c467c2231ba6ff2dd28b
Dale Terrell <tragicid@gmail.com>
2018-05-02 19:39:04 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  create_an_array = ["butt", "butts", "ass", "bottom"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "stupid"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array #unshift "Shut up"
 end
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array #pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  create_an_array #shift
 end
 
 def retrieve_element_from_index(array, index_number)
