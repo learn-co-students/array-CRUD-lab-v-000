0ebf3069cfa21ebbb878284d56839f8e27b8dfbb
Elaine Anderson <eparie@bu.edu>
2018-05-18 22:58:11 +0000

    Fixed

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,36 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = []
-  array << "arrays!"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-array = []
-array.unshift("wow")
+array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-array = ["arrays!"]
 array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["wow"]
   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-array = ["am", "ab", "ac"]
-array[0]
+array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-array = ["wow", "how", "cow"]
 array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["wow", "how", "arrays!"]
   array.last
 end
