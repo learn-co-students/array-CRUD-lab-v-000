8a7c3786557d6c0d9b5344fe43bc8d85db5bca35
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-28 17:16:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,29 +8,30 @@ end
 
 def add_element_to_end_of_array(array, element)
   colors = Array.new
-  colors.push(azul)
+  colors << "arrays!" 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   twelve = Array.new
+   twelve << "wow"
 end
 
 def remove_element_from_end_of_array(array)
-  
+   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+   array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+   array[-1]
 end
