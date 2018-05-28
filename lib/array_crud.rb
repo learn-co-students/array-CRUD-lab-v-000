556ff938afd74ce06d5e0dd6ec235d021a796106
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-28 17:07:28 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Nine", "Nueve", "Nein", "Neuf"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  colors = Array.new
+  colors.push(azul)
 end
 
 def add_element_to_start_of_array(array, element)
