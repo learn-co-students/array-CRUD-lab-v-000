48268d3f3f51b94da08c2a44a38af7e01b65ebe2
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-17 00:55:54 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  names = ["Jen", "Jenn", "Jenny", "Jennifer"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  names = ["Jen", "Jenn", "Jenny", "Jennifer"]
+  names << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  names = ["Jen", "Jenn", "Jenny", "Jennifer"]
+  names.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  names = ["Hong", "Jen", "Jenn", "Jenny", "Jennifer", "arrays!"]
+  arrays_names = names.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  names = ["wow", "Jen", "Jenn", "Jenny", "Jennifer", "Kimberly"]
+  wow_names = names.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  names = ["Hong", "am", "Jenn", "Jenny", "Jennifer", "Kimberly"]
+  names[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  names = ["wow", "Jen", "Jenn", "Jenny", "Jennifer", "Kimberly"]
+  names[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  names = ["Hong", "Jen", "Jenn", "Jenny", "Jennifer", "arrays!"]
+  names[-1]
 end
