56eadbdf09615280f697dbc07845352ad473c262
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-20 13:37:03 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  Array.new
+  []
 end
 
 def create_an_array
-  [1, 2, 3, 4]
+  array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -22,8 +22,8 @@ def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  array[index_number]
+def retrieve_element_from_index(array, index)
+  array[index]
 end
 
 def retrieve_first_element_from_array(array)
