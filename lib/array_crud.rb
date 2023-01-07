1e74c993abf5133c2b8a64607417c0a35bdae87d
lrh2daizzo <herrera.lrh@gmail.com>
2023-01-06 22:35:23 -0600

    done..

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["hi","hey","yo!","hello"]
+  ["dog","cat","snake","rabbit"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element
+  array << element 
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,15 +15,15 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop
+  array.pop 
 end
 
 def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  array[index_number]
+def retrieve_element_from_index(array, index)
+  array[index]
 end
 
 def retrieve_first_element_from_array(array)
